bf6ce4143353fcd91726e06480734c0e291a553f
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-02 13:15:39 -0600

    Add pseudocode for getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+"use strict"
+
+// Generate random number from 1 to 3
+// Assign strings "rock", "paper" and "scissors" to each number
+// Returns random string from the 3 assigned
+function getComputerChoice() {
+}
+
