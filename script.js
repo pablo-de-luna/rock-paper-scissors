e677932c62c6597572e3b343426c57161a30c853
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-02 14:03:46 -0600

    Create GetComputerChoice Re-write the funcion now with valid JS code This function returns computer choice

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 "use strict"
 
-// Generate random number from 1 to 3
-// Assign strings "rock", "paper" and "scissors" to each number
-// Returns random string from the 3 assigned
+// Generate random number from 0, 1 or 2
+// Assign "rock", "paper" and "scissors" to each number
+    // if 0 return "rock"
+    // if 1 return "paper"
+    // if 2 return "scissors"
+// Returns a string from the 3 assigned
 function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * 3);
+    
+    if (randomIndex === 0) {
+        return "rock";
+    } else if (randomIndex === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
+console.log( getComputerChoice() )
