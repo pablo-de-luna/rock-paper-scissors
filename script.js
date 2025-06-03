3981c0a152ca17e4ab9de8c7b166598ae80f3faa
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-02 20:18:12 -0600

    refactor comments

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const rock = "rock";
 const paper = "paper"
 const scissors = "scissors"
 
+// Function that gets Computer choice
 const ComputerChoice = () => {
     const randomIndex = Math.floor(Math.random() * 3);
     if (randomIndex === 0) {
@@ -15,13 +16,7 @@ const ComputerChoice = () => {
 }
 console.log("Computer's choice is:", ComputerChoice() )
 
-// ASK user a choice
-// CONVERT prompt to lower case
-// COMPARE prompt to valid choices
-    // IF it's not valid show message saying "Invalid choice"
-    // IF it's valid...
-    // RETURN valid choice "rock", "paper", or "scissors"
-
+// Function that gets Human choice
 const HumanChoice = () => {
     let UserChoice = prompt("Rock, paper or scissors, choose one:", "");
     UserChoice = UserChoice.toLowerCase();
