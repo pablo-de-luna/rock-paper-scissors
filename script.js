2e48d681ee92c0cddf86f0076a3dc3a2d1d6fd0e
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-02 19:34:38 -0600

    Assign option's variables

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 "use strict"
 
-// Generate random number from 0, 1 or 2
-// Assign "rock", "paper" and "scissors" to each number
-    // if 0 return "rock"
-    // if 1 return "paper"
-    // if 2 return "scissors"
-// Returns a string from the 3 assigned
+const rock = "rock";
+const paper = "paper"
+const scissors = "scissors"
+
+// GENERATE random number from 0, 1 or 2
+// ASSIGN "rock", "paper" and "scissors" to each number
+    // IF 0 return "rock"
+    // IF 1 return "paper"
+    // IF 2 return "scissors"
+// RETURN a string from the 3 assigned
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * 3);
     
     if (randomIndex === 0) {
-        return "rock";
+        return rock;
     } else if (randomIndex === 1) {
-        return "paper";
+        return paper;
     } else {
-        return "scissors";
+        return scissors;
     }
 }
-// console.log("Computer's choice is:", getComputerChoice() )
+console.log("Computer's choice is:", getComputerChoice() )
 
-// Ask user a choice
-// Convert prompt to lower case
-// Compare prompt to valid choices
-// Returns valid choice "rock", "paper", or "scissors"
+// ASK user a choice
+// CONVERT prompt to lower case
+// COMPARE prompt to valid choices
+    // IF it's not valid show message saying "Invalid choice"
+    // IF it's valid...
+    // RETURN valid choice "rock", "paper", or "scissors"
+
 function getHumanChoice() {
-    const UserChoice = prompt("Rock, paper or scissors, choose one:", "");
-
-    
+    let UserChoice = prompt("Rock, paper or scissors, choose one:", "");
+    UserChoice = UserChoice.toLowerCase();
+        // console.log(UserChoice);  
+    if (UserChoice !== rock || paper || scissors) {
+        alert("Invalid choice");
+    } else {
+        return console.log(UserChoice); 
+    }
 }
+
+console.log ( getHumanChoice() ); 
