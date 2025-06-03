71357f8011862d4c6883c614c5e4ce541cef1bcc
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-03 16:16:57 -0600

    Fix capitalized variable names

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
 
-// Function that gets Computer choice
-const ComputerChoice = () => {
+// Function that get Computer choice
+const computerChoice = () => {
     const randomIndex = Math.floor(Math.random() * 3);
     if (randomIndex === 0) {
         return rock;
@@ -15,10 +15,11 @@ const ComputerChoice = () => {
         return scissors;
     }
 }
-console.log("Computer's choice is:", ComputerChoice() );
+console.log("Computer's choice is:", computerChoice() );
 
+// TODO : make this function two separated functions 
 // Function that gets Human choice
-const HumanChoice = () => {
+const humanChoice = () => {
     let UserChoice = prompt("Rock, paper or scissors, choose one:", "");
     UserChoice = UserChoice.toLowerCase();
         // console.log(UserChoice); 
@@ -30,8 +31,17 @@ const HumanChoice = () => {
         return UserChoice; 
     }
 }
-console.log("User's choice is:", HumanChoice() );
+console.log("User's choice is:", humanChoice() );
 
 const humanScore = 0;
 const computerScore = 0;
 
+// GET human and computer choices 
+// ASSIGN numbers to rock, paper, scissors variables
+// COMPARE numbers to see who won the round
+// IF human won, call a function for this case
+// IF human computer, call a function for this case
+const playRound = () => {}
+
+
+playRound(com)
