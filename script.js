9b7405c842d365b3f0cb70aededa39c753821c17
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-03 20:49:43 -0600

    MAKE A MESS, START OVER AGAIN Feels good to start over with fresh eyes.

--- script.js
+++ script.js
@@ -1,64 +1,40 @@
 "use strict";
 
-// this function return number 0, 1 or 2
-function randomValue() {
-    return Math.floor(Math.random() * 3);
-}
-const computerChoiceValue = randomValue();
-
-// this function return choice name as string
-function computerChoice(choiceNum) {
-    if (choiceNum === 0) {
-        return "rock";
-    }
-    if (choiceNum === 1) {
-        return "paper";
-    }
-    if (choiceNum === 2) {
-        return "scissors";
-    }
+// this function return the computer choice
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random() * 3); 
+    const choiceName =
+        randomNum === 0 ? "rock" :
+        randomNum === 1 ? "paper" :
+        "scissors";
+    return choiceName; 
 }
 
 // Function that get Human choice in lower case
 function getHumanChoice() {
     let humanPrompt = prompt("Rock, paper or scissors, choose one:", "");
-    humanPrompt = humanPrompt.toLowerCase();
-    return humanPrompt;
+    return humanPrompt.toLowerCase();
 }
 
-// function that validates the prompt and returns it or return invalid
-function humanChoice(prompt) {
-    switch (prompt) {
+// function that return choice if its valid, return "invalid" if it's not the case
+function humanChoice(choice) {
+    switch (choice) {
         case "rock":
         case "paper":
         case "scissors":
-            return prompt;
+            return choice;
         default:
             return "invalid";
     }
 }
 
-console.log("Human choice is:", humanChoice( getHumanChoice() ) )
-console.log("Computer choice is:", computerChoice(computerChoiceValue) );
- 
-// const humanScore = 0;
-// const computerScore = 0;
+// console.log("Computer has selected:", getComputerChoice() );
+// console.log("Human has selected:", humanChoice( getHumanChoice() ) );
 
-// // GET human and computer choices 
-// // ASSIGN numbers to rock, paper, scissors variables
-// // COMPARE numbers to see who won the round
-// // IF human won, call a function for this case
-// // IF human computer, call a function for this case
-// // IF its a draw, display draw message
+console.log(
+`Human | ${humanChoice( getHumanChoice() )}
+ vs ${getComputerChoice()} | Computer`
+)
 
-// const playRound = (computerChoice, humanChoice) => {
-//     if (computerChoice > humanChoice) {
-//         // computer won function 
-//     } else if (computerChoice = humanChoice) {
-//         // its a draw
-//     } else {
-//         // human won function
-//     }
-// };
-
-// playRound (computerChoice(), humanChoice
+const humanScore = 0;
+const computerScore = 0;
