51f17ffe0893dcce6aff2508a46d405d974a35c7
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-04 19:34:12 -0600

    Write code to increment score

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// this function return the computer choice
+// Function that return the computer choice
 function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3); 
     if (randomNum === 0) {
@@ -17,7 +17,7 @@ function getHumanChoice() {
     let input = prompt("Rock, paper or scissors, choose one:", "");
     return humanChoiceValidation( input.toLowerCase() );
 }
-// function that return VALID human choice, return "invalid" if it's not
+// Function that return VALID human choice, return "invalid" if it's not
 function humanChoiceValidation(choice) {
     if (choice === "rock" ||
         choice === "paper" ||
@@ -28,42 +28,32 @@ function humanChoiceValidation(choice) {
     }
 }
 
-const humanScore = 0;
-const computerScore = 0;
-
-// GET human and computer choice
-// CHECK if human choice beat computer choice
-    // IF rock vs scissors or
-    //    paper vs rock or
-    //    scissors vs paper
-        // SHOW message "Human won this round"
-        // RETURN human score + 1
-    // ELSE
-        // SHOW message "Computer won this round"
-        // RETURN computer score + 1
+let humanScore = 0;
+let computerScore = 0;
         
+// Function that log who won / draw. And returns
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock" && computerChoice === "scissors" ||
         humanChoice === "paper" && computerChoice === "rock" ||
         humanChoice === "scissors" && computerChoice === "paper") {
-        console.log(`You WIN!, ${humanChoice} beat ${computerChoice}`)
-        // return humanScore + 1;
+        console.log(`You WIN!, ${humanChoice} beat ${computerChoice}`);
+        humanScore += 1;
     } else if (humanChoice === computerChoice) {
-        console.log("It's a DRAW")
+        console.log("It's a DRAW");
+    } else if (humanChoice === "invalid") {
+        console.log(`INVALID INPUT, what's a ${humanSelection}`);
     } else {
-        console.log(`You LOSE!, ${computerChoice} beat ${humanChoice}`)
-        // return computerScore + 1;
+        console.log(`You LOSE!, ${computerChoice} beat ${humanChoice}`);
+        computerScore += 1;
     }
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-console.log("human selection is =", humanSelection);
-console.log("computer selection is =", computerSelection);
+playRound(humanSelection, computerSelection);
+
+// console.log("human choose =", humanSelection);
+// console.log("computer choose =", computerSelection);
 console.log("human score is =", humanScore);
 console.log("computer score is =", computerScore);
-
-// console.log( playRound() )
-
-playRound(humanSelection, computerSelection);
