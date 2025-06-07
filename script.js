a863a6bde913fcd804f0b3cb8ccab80facdfcc09
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-06 19:29:04 -0600

    Change indentation to two spaces

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 "use strict";
 
 const getComputerChoice = () => {
-    const randomNum = Math.floor(Math.random() * 3); 
-    if (randomNum === 0) {
-        return 'rock';
-    }
-    if (randomNum === 1) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
+  const randomNum = Math.floor(Math.random() * 3); 
+  if (randomNum === 0) {
+     return 'rock';
+  }
+  if (randomNum === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
 };
 
 const getHumanChoice = () => {
-    const input = prompt(`ROUND ${roundCount + 1} | Rock, paper or scissors`);
-    return validateHumanChoice( input.toLowerCase() );
+  const input = prompt(`ROUND ${roundCount + 1} | Rock, paper or scissors`);
+  return validateHumanChoice( input.toLowerCase() );
 }
 
 const validateHumanChoice = choice => {
-    if (choice === 'rock' ||
-        choice === 'paper' ||
-        choice === 'scissors') {
-        return choice;
-    } else {
-        return 'invalid';
-    }
+  if (choice === 'rock' ||
+    choice === 'paper' ||
+    choice === 'scissors') {
+    return choice;
+  } else {
+    return 'invalid';
+  }
 };
 
 const numberOfRounds = 5;
@@ -33,41 +33,43 @@ let computerScore = 0;
 let roundCount = 0;
 
 const playRound = (humanChoice, computerChoice) => { 
-    setupRound();
-    
-    if (humanChoice === 'invalid') {
-        console.log('INVALID INPUT, It\'s a DRAW');
-        return;
-    }
-    if (humanChoice === computerChoice) {
-        console.log('It\'s a DRAW');
-        return;
-    }
-    if (humanChoice === 'rock' && computerChoice === 'scissors' ||
-        humanChoice === 'paper' && computerChoice === 'rock' ||
-        humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log(`You WIN!, ${humanChoice} beat ${computerChoice}`);
-        humanScore += 1;
-    } else {
-        console.log(`You LOSE!, ${computerChoice} beat ${humanChoice}`);
-        computerScore += 1;
-    }
+  setupRound();
+  
+  if (humanChoice === 'invalid') {
+    console.log('INVALID INPUT, It\'s a DRAW');
+    return;
+  }
+  if (humanChoice === computerChoice) {
+    console.log('It\'s a DRAW');
+    return;
+  }
+  if (
+    (humanChoice === 'rock' && computerChoice === 'scissors')
+    || (humanChoice === 'paper' && computerChoice === 'rock')
+    || (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    console.log(`You WIN!, ${humanChoice} beat ${computerChoice}`);
+    humanScore += 1;
+  } else {
+    console.log(`You LOSE!, ${computerChoice} beat ${humanChoice}`);
+    computerScore += 1;
+  }
 };
 
 const setupRound = () => {
-    console.clear();
-    roundCount += 1;
-    console.log( 'ROUND', roundCount.toString() );
+  console.clear();
+  roundCount += 1;
+  console.log( 'ROUND', roundCount.toString() );
 };
 
 // Repeat itself the number of rounds declared in numberOfRounds const
 const playGame = function playGame() {
-    if (roundCount < numberOfRounds) {
-        playRound( getHumanChoice(), getComputerChoice() );
-        playGame();
-    } else {
-        return;
-    }
+  if (roundCount < numberOfRounds) {
+    playRound( getHumanChoice(), getComputerChoice() );
+    playGame();
+  } else {
+    return;
+  }
 };
 
 alert('ROCK, PAPER SCISSORS!')
@@ -75,11 +77,11 @@ playGame();
 console.clear();
 
 if (humanScore > computerScore) {
-    console.log('YOU WIN THE GAME')
+  console.log('YOU WIN THE GAME')
 } else if (humanScore === computerScore) {
-    console.log('IT\'S A DRAW')
+  console.log('IT\'S A DRAW')
 } else {
-    console.log('YOU LOSE THE GAME');
+  console.log('YOU LOSE THE GAME');
 }
 
 console.log(`You got ${ humanScore.toString() } wins`);
