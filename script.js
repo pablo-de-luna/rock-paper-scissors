1b1ed7ff71460f2a1063624c25a2b4d1e38724f5
Pablo De Luna <205702351+pablo-de-luna@users.noreply.github.com>
2025-06-30 13:56:40 -0600

    Add event listener function to preview hand choice

--- script.js
+++ script.js
@@ -11,23 +11,30 @@ const getHumanChoice = () => {
     return validateHumanChoice( input.toLowerCase() );
 }
 
-const rockButton = document.querySelector("#rock-button");
-const paperButton = document.querySelector("#paper-button");
-const scissorsButton = document.querySelector("#scissors-button");
-
 const numberOfRounds = 5;
 let humanScore = 0;
 let computerScore = 0;
 let roundCount = 0;
 
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
+
 rockButton.addEventListener("click", () => {
     console.log("rock");  
 });
 
-paperButton.addEventListener("click", () => {
-    console.log("paper");  
+const leftHand = document.querySelector("#hand-left-space > img");
+
+rockButton.addEventListener("mouseover", () => {
+    leftHand.src = "./assets/images/rock-hand.png"
 });
 
-scissorsButton.addEventListener("click", () => {
-    console.log("scissors");  
+paperButton.addEventListener("mouseover", () => {
+    leftHand.src = "./assets/images/paper-hand.png"
 });
+
+scissorsButton.addEventListener("mouseover", () => {
+    leftHand.src = "./assets/images/scissors-hand.png"
+});
+
